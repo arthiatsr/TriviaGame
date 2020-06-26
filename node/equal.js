73865543ea9ae5303d0de3ea037400a3eb736aab
Arthi KS <arthiatsr@gmail.com>
2020-06-25 21:45:55 -0500

    Delete equal.js

--- node/equal.js
+++
@@ -1,9 +0,0 @@
-var num1 = parseInt(process.argv[2]);
-var num2 = parseInt(process.argv[3]);
-
-if (num1===num2){    
-    console.log(process.argv[4]);
-} else {
-    
-    console.log(process.argv[5]);
-}
